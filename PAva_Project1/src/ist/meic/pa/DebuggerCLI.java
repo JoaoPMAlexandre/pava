bb9ced64d40ec869d6714672e626acea1592dbf1
Miguel Rebocho de Oliveira <rebochodeoliveira.miguel@gmail.com>
2015-03-21 17:22:35 +0000

    Criaçao do metodo main do debugger.

--- PAva_Project1/src/ist/meic/pa/DebuggerCLI.java
+++ PAva_Project1/src/ist/meic/pa/DebuggerCLI.java
@@ -1,58 +1,39 @@
 package ist.meic.pa;
 
-import java.util.Scanner;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class DebuggerCLI {
 	
 	public static void main(String[] args) {
-		try{
-		
-		}catch (Throwable ex) {
-			System.out.println("Welcome to Debugger:");
-			System.out.println("Tips: 1-Abort , 2-Info , 3-Throw , 4-Return<value>, 5-Get<field name>, 6-Set<field name><new value>, 7-Retry");
-			System.out.println("Which one operation do you want to do:");
-			Scanner sc = new Scanner(System.in);
-		    int i = sc.nextInt();
-		    DebuggerCLI debugg = new DebuggerCLI();
-		    
-		    switch(i){
-		    
-		    	case 1: debugg.abort();
-		    			break;
-		    
-		    	case 2: debugg.info();
-		    			break;
-		    	
-		    	case 3: debugg.throwagain();
-		    			break;
-		    			
-		    	case 4: System.out.println("What value do you wanna return?");
-		    			int ret = sc.nextInt();
-		    			debugg.returnval(ret);
-		    			break;
-		    			
-		    	case 5: System.out.println("What field name do you wanna get?");
-    					String gets = sc.next();
-    					debugg.getfield(gets);
-    					break;
-    					
-		    	case 6: System.out.println("What field name do you wanna set?");
-						String sets = sc.next();
-						System.out.println("Which value?");
-						String vals = sc.next();
-						debugg.setfield(sets,vals);
-						break;
-						
-		    	case 7: debugg.retry();
-		    			break;
-		    }
-			
+		try {
+			Class program = Class.forName(args[0]);
+			Method program_main = program.getMethod("main", null);
+			program_main.setAccessible(true);
+			program_main.invoke(program, null);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
 	public void abort(){
-		
-		
+			
 	}
 
 	public void retry() {
@@ -84,9 +65,4 @@ public class DebuggerCLI {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	
-	
-
 }
